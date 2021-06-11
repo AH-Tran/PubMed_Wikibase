18080653458f153cc5e5581e20f72f25ce4f1fbd
AH-Tran <AH_Tran@hotmail.de>
2021-06-11 15:30:32 +0200

    Corrected Ingestion

--- scripts/create_items_wd.py
+++ scripts/create_items_wd.py
@@ -6,23 +6,23 @@ from wikidataintegrator import wdi_core, wdi_login
 import pandas as pd
 import pprint
 
-def upload_data():
+def upload_data(login_instance, config):
     # load excel table to load into Wikibase
     mydata = pd.read_csv("pubmed_data.csv")
     for index, row in mydata.iterrows():
         ## Prepare the statements to be added
         item_statements = [] # all statements for one item
-        item_statements.append(wdi_core.WDString("column1_value", prop_nr="P11")) #title 
-        item_statements.append(wdi_core.WDString("column1_value", prop_nr="P32")) #abstract 
-        item_statements.append(wdi_core.WDString("column1_value", prop_nr="P12")) #author
-        item_statements.append(wdi_core.WDString("column1_value", prop_nr="P5"))  #Pubmed ID
-        item_statements.append(wdi_core.WDString("column1_value", prop_nr="P14")) #publication date
-        item_statements.append(wdi_core.WDString("column1_value", prop_nr="P29")) #MeSH Descriptor ID
+        item_statements.append(wdi_core.WDString("title", prop_nr="P11")) #title 
+        item_statements.append(wdi_core.WDString("abstract", prop_nr="P32")) #abstract 
+        item_statements.append(wdi_core.WDString("author", prop_nr="P12")) #author
+        item_statements.append(wdi_core.WDString("pubmed id", prop_nr="P5"))  #Pubmed ID
+        item_statements.append(wdi_core.WDString("publication date", prop_nr="P14")) #publication date
+        item_statements.append(wdi_core.WDString("mesh descriptor id", prop_nr="P29")) #MeSH Descriptor ID
         #item_statements.append(wdi_core.WDItem("Q1234", prop_nr="P2"))
         #item_statements.append(wdi_core.WDURL("<http://someURL>", prop_nr="P3"))
 
         ## instantiate the Wikibase page, add statements, labels and descriptions
-        wbPage = wdi_core.WDItemEngine(data=item_statements, mediawiki_api_url="<wikibaseURL>/w/api.php")
+        wbPage = wdi_core.WDItemEngine(data=item_statements, mediawiki_api_url=config.wikibase_url + "/w/api.php")
         wbPage.set_label("label", lang="en")
         #wbPage.set_label("Kennzeichen", lang="de")
         wbPage.set_description("description", lang="en")
