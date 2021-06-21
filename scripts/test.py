81b4769dd264dfe0726c033638b6c2405dd0d802
AH-Tran <AH_Tran@hotmail.de>
2021-06-21 13:05:57 +0200

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as Xet
 import pandas as pd
   
 mydata = pd.read_csv("pubmed_data.csv")
-#print(mydata.loc[0].at['PubmedArticle_MedlineCitation_Owner'])
+print(mydata.loc[0].at['PubmedArticle_MedlineCitation_Owner'])
 #print(mydata.at[0,'PubmedArticle_MedlineCitation_Owner'])
 for index, row in mydata.iterrows():
     print(mydata.loc[index].at['PubmedArticle_MedlineCitation_Article_ArticleTitle'])
