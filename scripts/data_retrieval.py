e79adb8f8ff491afb8061c68ba81fea3fbfd818b
AH-Tran <AH_Tran@hotmail.de>
2021-07-20 16:40:06 +0200

    Testing json

--- scripts/data_retrieval.py
+++ scripts/data_retrieval.py
@@ -7,10 +7,14 @@ import re
 import os
 import xmltodict
 import ast
+import pandas as pd
 from wikibaseintegrator import wbi_core, wbi_login
 import spacy
 import scispacy
 from scispacy.linking import EntityLinker
+
+data_path = os.path.abspath('../data/')
+#file_path = os.path.
 
 #function to get a list of all jsondicts
 def IDacq(queryterm: str):
@@ -122,12 +126,55 @@ def dataaquisition(queryterm):
             dicts.append(jsondict)
     return dicts
 
+def MeSHTermDF(queryterm):
+    nlp = spacy.load("en_core_sci_sm")
+    nlp.add_pipe("scispacy_linker", config={"resolve_abbreviations": True, "linker_name": "mesh"})
+    IDLIST = IDacq(queryterm=queryterm)
+    for ID in IDLIST:
+        print("Getting Article: " + ID)
+        mdrequest = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=" + ID + "&retmode=xml")
+        print(ID + " done")
+        mdxml = ET.fromstring(mdrequest.text)
+        f = mdxml.findall("*//MeshHeading/DescriptorName")
+        dflist =[]
+        if len(f) == 0:
+            print("Keine Meshterms vorhanden")
+            abstracts = ['.//AbstractText','.//Keyword','.//ArticleTitle']
+            abstract = ""
+            for tag in abstracts:
+                for child in mdxml.findall(tag):
+                        abstract+=str(child.text)
+                        abstract+=" "
+            doc = nlp(abstract)
+            linker = nlp.get_pipe("scispacy_linker")
+            entity = doc.ents
+            for i in entity:
+                for mesh_ent in i._.kb_ents:
+                    meshinfo = linker.kb.cui_to_entity[mesh_ent[0]]
+                    meshinfo = list(meshinfo)
+                    del meshinfo[2:4]
+                    dflist.append(meshinfo)
+        elif len(f) > 0:
+            print(ID, "MeshTerms vorhanden!")
+    df = pd.DataFrame(dflist,columns=['MeSH Unique ID','MeSH Heading','ScopeNote'])
+    df = df.drop_duplicates(subset=['MeSH Unique ID'])
+    df['MeSHBrowserLink'] = 'https://meshb.nlm.nih.gov/record/ui?ui=' + df['MeSH Unique ID']
+    return df
+
 if __name__ == '__main__':
-    result = dataaquisition('infectious diseases') # type: list containing dicts as items
+    result = dataaquisition('infectious diseases') # type list containing dicts as items
+    #for i in result:
     #print("Type:")
     #print(result[1])
-    #with open('result1.json', 'w') as fp:
-    #    json.dump(result[1], fp)
+        #with open(os.path.join(os.path.dirname(__file__), data_path), 'w') a file:
+    #for i in result:
+    #    with open(data_path,i +'.json', 'w') as fp:
+    #        json.dump(result[i], fp, indent=4)
+
+    with open('result0.json', 'w') as fp:
+            json.dump(result[0], fp, indent=4)
+    with open('result1.json', 'w') as fp:
+            json.dump(result[1], fp, indent=4)
     #print(len(result))
     #print(result)
     #with open('pubmed_data.json','w') as file:
--- scripts/test.py
+++ scripts/test.py
@@ -1,16 +1,32 @@
 # Importing the required libraries
 import xml.etree.ElementTree as Xet
 import pandas as pd
-  
-mydata = pd.read_csv("pubmed_data.csv")
-print(mydata.loc[0].at['PubmedArticle_MedlineCitation_Owner'])
-#print(mydata.at[0,'PubmedArticle_MedlineCitation_Owner'])
-for index, row in mydata.iterrows():
-    print(mydata.loc[index].at['PubmedArticle_MedlineCitation_Article_ArticleTitle'])
-    #print(index)
-#    print(row)
-    print("*** END ITERATION ***")
-#print(mydata.iloc[0]['PubmedArticle_MedlineCitation_Article_ArticleTitle'])
+# importing the module
+import json
 
+def test():
+    mydata = pd.read_csv("pubmed_data.csv")
+    print(mydata.loc[0].at['PubmedArticle_MedlineCitation_Owner'])
+    #print(mydata.at[0,'PubmedArticle_MedlineCitation_Owner'])
+    for index, row in mydata.iterrows():
+        print(mydata.loc[index].at['PubmedArticle_MedlineCitation_Article_ArticleTitle'])
+        #print(index)
+    #    print(row)
+        print("*** END ITERATION ***")
+    #print(mydata.iloc[0]['PubmedArticle_MedlineCitation_Article_ArticleTitle'])
 
+def jsoninsert():
+    # Opening JSON file
+    with open('result0.json') as json_file:
+        data0 = json.load(json_file)
+    with open('result1.json') as json_file:
+        data1 = json.load(json_file)
+        # Print the type of data variable
+        print("Type:", type(data0))
+        print(data0)
+    data0.update(data1)
+    with open('result_union.json', 'w') as fp:
+            json.dump(data0, fp, indent=4)
 
+if __name__ == '__main__':
+    jsoninsert()
